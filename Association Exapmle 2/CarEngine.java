59f00314dfc0449d0b08e8b07e8343af53b86718
VorugantiSaiKrishna <92724431+VorugantiSaiKrishna@users.noreply.github.com>
2021-10-26 08:09:32 +0530

    Delete Association Exapmle 2 directory

--- Association Exapmle 2/CarEngine.java
+++
@@ -1,7 +0,0 @@
-package Association2;
-
-public class CarEngine {
-  public void startEngine() {
-	  System.out.println("Engine Started");
-  }
-}
--- Association Exapmle 2/MusicPlayer.java
+++
@@ -1,7 +0,0 @@
-package Association2;
-
-public class MusicPlayer {
-  public void MusicPlayer() {
-	  System.out.println("car has music player");
-  }
-}
