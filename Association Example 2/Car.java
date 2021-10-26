cee808ff8e2c506007f35a948182972b5323350b
VorugantiSaiKrishna <92724431+VorugantiSaiKrishna@users.noreply.github.com>
2021-10-26 08:12:34 +0530

    Add files via upload

---
+++ Association Example 2/Car.java
@@ -0,0 +1,21 @@
+package Association2;
+
+public class Car {
+   private String color;
+   private int speed;
+public String getColor() {
+	return color;
+}
+public void setColor(String color) {
+	this.color = color;
+}
+public int getSpeed() {
+	return speed;
+}
+public void setSpeed(int speed) {
+	this.speed = speed;
+}
+   
+   
+   
+}
